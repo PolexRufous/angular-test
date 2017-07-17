c0fe96de6899bbb5190cf6d97762de38c62b354f
PolexRufous <polex.rufous@gmail.com>
2017-07-18 00:23:37 +0300

    Added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,11 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { StorageService } from './shared/services/storage.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +26,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,8 @@ import { FooterComponent } from './footer/footer.component';
     HttpModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [StorageService],
   bootstrap: [AppComponent]
